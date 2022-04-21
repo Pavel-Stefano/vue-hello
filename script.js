ac9c614c3323fb17f4183ab90628874bbce1bdbf
Pavel-Stefano <pavelstefanominuti@gmail.com>
2022-04-21 20:10:54 +0200

    immagini random

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
+
+
 const app = new Vue({
-    el:'#app',
+    el: '#root',
     data: {
-        titolo: "io sono il titolo"
+      messaggio: "Welcome Vue",
+      immagine: "https://picsum.photos/id/",
+      idImmagine: "",
+      imgSize: "/600/600",
     },
     methods: {
 
+        getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            let random = Math.floor(Math.random() * (max - min) + min);
+            console.log(random);
+            this.idImmagine= random;
+          },
     },
-})
+    
+  }) 
+  
